8c26bf709f7cc6956a423bfd2e5497df1c14caa7
David Mendes <david.f.mendes@proton.me>
2023-03-23 22:29:14 +0000

    Added LP1Semana04.sln and Power2Method Project.

---
+++ Power2Method/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Power2Method
+{
+    class Program
+    {
+        private static void PowerOf2Until5()
+        {
+            for (int i = 1; i <= (1 << 5); i = i << 1)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        static void Main()
+        {
+            PowerOf2Until5();
+            PowerOf2Until5();
+        }
+    }
+}
