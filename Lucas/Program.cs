94e7f37df3d9675bc1384b0a59fcfa7402ef37d1
David Mendes <david.f.mendes@proton.me>
2023-03-24 18:01:27 +0000

    Added Lucas Project

---
+++ Lucas/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lucas
+{
+    class Program
+    {
+        private static int Lucas(int n)
+        {
+            int answer = 2;
+            
+            
+            if (n == 1)
+            {
+                answer = n;
+            }
+            else if (n > 1)
+            {
+                answer = Lucas(n - 1) + Lucas(n - 2);
+            }
+
+            return answer;
+        }
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Insert number to find in Lucas' sequence");
+            int lucasNum = Convert.ToInt16(Console.ReadLine());
+            
+            Console.WriteLine(Lucas(lucasNum));
+        }
+    }
+}
