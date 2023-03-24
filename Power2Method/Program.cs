e74e74acf9aec076eb0bdb33bfaa41ad90d1b243
David Mendes <david.f.mendes@proton.me>
2023-03-24 17:34:31 +0000

    Added commentary about PowersOf2Until5, PowersOf2UntilN and Main methods

--- Power2Method/Program.cs
+++ Power2Method/Program.cs
@@ -4,11 +4,17 @@ namespace Power2Method
 {
     class Program
     {
+        /// <summary>
+        /// Returns each power of 2 in a separate line until the 5th
+        /// </summary>
         private static void PowersOf2Until5()
         {
             PowersOf2UntilN(5);
         }
-
+        /// <summary>
+        /// Returns each power of 2 in a separate line until the nth
+        /// </summary>
+        /// <param name="n">Defines the last power of 2 to return</param>
         private static void PowersOf2UntilN(int n)
         {
             for (int i = 1; i <= (1 << n); i = i << 1)
@@ -16,6 +22,9 @@ namespace Power2Method
                 Console.WriteLine(i);
             }
         }
+        /// <summary>
+        /// Calls PowersOf2Until5 two times followed by calling PowersOf2UntilN two times with n = 3 and 4, respectively
+        /// </summary>
         static void Main()
         {
             PowersOf2Until5();
